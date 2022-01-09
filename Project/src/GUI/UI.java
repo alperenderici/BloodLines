2d742d741802a9847060d4e2bac9e0e8d18bfa90
gizemhacioglu <64923120+gizemhacioglu@users.noreply.github.com>
2022-01-09 16:20:21 +0300

    adding labels of second panel

--- Project/src/GUI/UI.java
+++ Project/src/GUI/UI.java
@@ -9,6 +9,22 @@ public class UI {
     JPanel firstPanel;
     JPanel secondPanel;
     JPanel thirdPanel;
+    JButton buttonEkle;
+    JButton buttonTemizle;
+    JButton buttonOlustur;
+    JLabel labelAd;
+    JLabel labelSoyAd;
+    JLabel labelDogumTarihi;
+    JLabel labelCinsiyet;
+    JLabel labelAkraba;
+    JTextField textFieldAd;
+    JTextField textFieldSoyad;
+    JTextField textFieldDogumTarihi;
+    JComboBox comboBoxCinsiyet;
+    String[] cinsiyet = {"Kadın","Erkek"};
+    JComboBox comboBoxAkraba;
+    String[] akraba = {"Anne","Baba","Kardeş","Amca","Teyze","Dayı","Hala","Eş"};
+
     Border border = BorderFactory.createLineBorder(Color.black);
 
     UI(){
@@ -16,30 +32,37 @@ public class UI {
         firstPanel.setBorder(border);
         firstPanel.setBounds(0, 0, 1100, 800);
         firstPanel.setLayout(null);
-
         secondPanel = new JPanel();
         secondPanel.setBorder(border);
         secondPanel.setBounds(1100, 0, 430, 400);
         secondPanel.setLayout(null);
-
         thirdPanel = new JPanel();
         thirdPanel.setBorder(border);
         thirdPanel.setBounds(1100, 400, 430, 400);
         thirdPanel.setLayout(null);
+
+        labelAd = new JLabel();
+        labelAd.setText("Ad: ");
+
+        labelSoyAd = new JLabel();
+        labelSoyAd.setText("Soyad: ");
+
+        labelDogumTarihi = new JLabel();
+        labelDogumTarihi.setText("Doğum Tarihi: ");
+
+        labelCinsiyet = new JLabel();
+        labelCinsiyet.setText("Cinsiyet: ");
 
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(null);
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
         frame.setVisible(true);
-
         frame.add(firstPanel);
         frame.add(secondPanel);
         frame.add(thirdPanel);
     }
-
     public static void main(String[] args) {
         UI ui = new UI();
-
     }
 }
